ce15f188783c3d957ea74e7761e6e5668b31a509
root <sujanbarman12345q@gmail.com>
2020-06-13 21:10:35 +0000

    some minor changes

--- content/views.py
+++ content/views.py
@@ -3,13 +3,13 @@ from .models import About, Work, Comments, Social
 
 
 def home(request):
-    about = About.objects.all()
+    about = About.objects.all()[0]
     works = Work.objects.all()
     comments = Comments.objects.all()
-    social = Social.objects.all()
+    social = Social.objects.all()[0]
     return render(request, 'main.html', {
-        'about': about,
+        'profile': about,
         'works': works,
         'comments': comments,
-        'social': social,
+        'url': social,
     })
