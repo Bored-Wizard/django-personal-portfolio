04754041e6a4f0507e97dea56f16a2a21abf41fe
root <sujanbarman12345q@gmail.com>
2020-06-25 05:07:30 +0000

    ok

--- myport/settings.py
+++ myport/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pportfolio9@gmail.com'
-EMAIL_HOST_PASSWORD = 'sujan123'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PIRT = 587
 EMAIL_USE_TLS = True
 
